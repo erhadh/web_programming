12cc3c750b74ca77c78866398f4c2873216574f2
Erhad Husovic <erhad.husovic@stu.ibu.edu.ba>
2024-03-22 17:32:00 +0100

    use spapp for the main views

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $("main#spapp > section").height($(document).height() - 60);
+
+  //var app = $.spapp({pageNotFound : 'error_404'}); // initialize
+
+  var app = $.spapp({
+    defaultView  : "#main",
+    templateDir  : "./tpl/",
+    //pageNotFound : "error_404"
+  });
+
+  // define routes
+  app.route({
+    view: 'main',
+    onCreate: function() {},
+    onReady: function() { },
+    load: "index.html"
+  });
+  
+  app.route({view: 'login', load: 'login.html' });
+  app.route({view: 'items', load: 'items.html' });
+  app.route({view: 'warehouses', load: 'warehouses.html' });
+  app.route({view: 'orders', load: 'orders.html' });
+  app.route({view: 'suppliers', load: 'suppliers.html' });
+
+  app.run();
+});
