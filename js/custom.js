2c96715282c7bfbee863e395604e7ec7f100b391
Erhad Husovic <erhad.husovic@stu.ibu.edu.ba>
2024-04-01 22:59:19 +0200

    add login AJAX

--- js/custom.js
+++ js/custom.js
@@ -18,12 +18,122 @@ $(document).ready(function() {
     load: "index.html"
   });
   
-  app.route({view: 'login', load: 'login.html' });
-  app.route({view: 'items', load: 'items.html' });
-  app.route({view: 'warehouses', load: 'warehouses.html' });
-  app.route({view: 'orders', load: 'orders.html' });
-  app.route({view: 'suppliers', load: 'suppliers.html' });
-  app.route({view: 'istatus', load: 'items_status.html' });
+  app.route({
+    view: 'login', 
+    load: 'login.html'
+  });
+
+  app.route({
+    view: 'register', 
+    load: 'register.html'
+  });
+
+  app.route({
+    view: 'items', 
+    load: 'items.html',
+    onCreate: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    },
+    onReady: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    } 
+  });
+
+  app.route({
+    view: 'warehouses', 
+    load: 'warehouses.html',
+    onCreate: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    },
+    onReady: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    } 
+  });
+
+  app.route({
+    view: 'orders', 
+    load: 'orders.html',
+    onCreate: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    },
+    onReady: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    } 
+  });
+
+  app.route({
+    view: 'suppliers', 
+    load: 'suppliers.html',
+    onCreate: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    },
+    onReady: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    } 
+  });
+
+  app.route({
+    view: 'istatus', 
+    load: 'items_status.html',
+    onCreate: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    },
+    onReady: function() {
+      var datatablesSimple = document.getElementById('datatablesSimple');
+      if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+      }
+    } 
+  });
 
   app.run();
 });
+
+function loginUser() {
+  let xhttp = new XMLHttpRequest();
+  let filename = "userdata.json";
+  xhttp.open("GET", filename);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      let username = document.getElementById("inputEmail");
+      let password = document.getElementById("inputPassword");
+      let jsonObj = JSON.parse(xhttp.responseText);
+      if (username.value !== jsonObj.username || 
+        password.value !== jsonObj.password) {
+          alert("Wrong credentials!");
+      } else {
+          alert("Successfully logged in! Redirecting to dashboard.");
+          window.location.replace("#main");
+      }
+    }
+  };
+  xhttp.send();
+}
+
